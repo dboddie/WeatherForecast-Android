e81f10abca263fc66b38828dcb3aa10c34801105
David Boddie <david@boddie.org.uk>
2018-09-19 01:33:17 +0200

    Refactored the network code to run in a separate thread in order to make the application behave properly when built for newer SDK versions.

--- src/weatherforecast.py
+++ src/weatherforecast.py
@@ -21,6 +21,7 @@ from java.io import BufferedInputStream, FileNotFoundException, InputStream
 from java.lang import Object, String, System
 from java.net import HttpURLConnection, URL
 from java.util import List, Map
+from android.os import AsyncTask
 from android.widget import Toast
 from serpentine.activities import Activity
 
@@ -58,12 +59,44 @@ class WeatherForecastActivity(Activity):
     
     def locationEntered(self, location):
     
-        #stream = self.getSampleStream()
-        try:
-            forecasts = self.fetchData(location)
-        except WeatherException, e:
-            Toast.makeText(self, e.getMessage(), Toast.LENGTH_SHORT).show()
+        if self.state == "fetching":
             return
+        
+        self.current_time = System.currentTimeMillis()
+        self.place = location
+        
+        try:
+            item = self.cache[location]
+            if self.current_time - item.time < 600000: # 10 minutes
+                self.showForecasts(item.forecasts)
+                return
+        
+        except KeyError:
+            pass
+        
+        self.state = "fetching"
+        
+        self.task = Task(self)
+        self.task.execute(array([location]))
+        #self.parseForecasts(self.getSampleStream())
+    
+    @args(void, [InputStream])
+    def parseForecasts(self, stream):
+    
+        if stream == None:
+            self.showError()
+            self.state = "entry"
+            return
+        
+        forecasts = self.parser.parse(stream)
+        stream.close()
+        
+        self.showForecasts(forecasts)
+    
+    @args(void, [List(Forecast)])
+    def showForecasts(self, forecasts):
+    
+        self.cache[self.place] = CacheItem(self.current_time, forecasts)
         
         try:
             self.forecastWidget.addForecasts(forecasts)
@@ -72,8 +105,13 @@ class WeatherForecastActivity(Activity):
             self.setContentView(self.forecastWidget)
         
         except:
-            Toast.makeText(self, "Failed to read weather forecast,",
-                           Toast.LENGTH_SHORT).show()
+            self.state = "entry"
+            self.showError()
+    
+    def showError(self):
+    
+        Toast.makeText(self, "Failed to read weather forecast,",
+                       Toast.LENGTH_SHORT).show()
     
     @args(InputStream, [])
     def getSampleStream(self):
@@ -81,48 +119,61 @@ class WeatherForecastActivity(Activity):
         resources = self.getResources()
         return resources.openRawResource(R.raw.sample)
     
-    @args(List(Forecast), [String])
-    def fetchData(self, place):
-    
-        current_time = System.currentTimeMillis()
-        
-        try:
-            item = self.cache[place]
-            if current_time - item.time < 600000: # 10 minutes
-                return item.forecasts
-        
-        except KeyError:
-            pass
-        
-        url = URL("https://www.yr.no/place/" + place + "/forecast.xml")
-        connection = CAST(url.openConnection(), HttpURLConnection)
-        connection.setInstanceFollowRedirects(True)
-        
-        #length = connection.getContentLength()
-        #if length <= 0:
-        #    raise WeatherException("No connection")
-        
-        try:
-            stream = BufferedInputStream(connection.getInputStream())
-        except:
-            raise WeatherException("Resource not found")
-        
-        forecasts = self.parser.parse(stream)
-        stream.close()
-        
-        self.cache[place] = CacheItem(current_time, forecasts)
-        
-        return forecasts
-    
     def onBackPressed(self):
     
         if self.state == "forecast":
             # Return to the entry widget.
             self.state = "entry"
             self.setContentView(self.entryWidget)
-        else:
+        
+        elif self.state == "entry":
             # If already showing the entry widget then exit.
             Activity.onBackPressed(self)
+
+
+class Task(AsyncTask):
+
+    #               Params Progress Result
+    __item_types__ = [str, int, InputStream]
+    
+    @args(void, [WeatherForecastActivity])
+    def __init__(self, activity):
+    
+        AsyncTask.__init__(self)
+        self.activity = activity
+    
+    @args(Result, [[Params]])
+    def doInBackground(self, params):
+    
+        # Unpack the location from the array.
+        location = params[0]
+        
+        try:
+            forecasts = self.fetchData(location)
+        except WeatherException, e:
+            Toast.makeText(self.activity, e.getMessage(), Toast.LENGTH_SHORT).show()
+            return []
+        
+        return forecasts
+    
+    @args(InputStream, [str])
+    def fetchData(self, place):
+    
+        url = URL("https://www.yr.no/place/" + place + "/forecast.xml")
+        connection = CAST(url.openConnection(), HttpURLConnection)
+        connection.setInstanceFollowRedirects(True)
+        
+        try:
+            stream = BufferedInputStream(connection.getInputStream())
+        except:
+            return None
+        
+        return stream
+    
+    @args(void, [Result])
+    def onPostExecute(self, stream):
+    
+        self.activity.parseForecasts(stream)
 
 
 class CacheItem(Object):
@@ -136,4 +187,3 @@ class CacheItem(Object):
         
         self.time = time
         self.forecasts = forecasts
-
